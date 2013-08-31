964706fa62ab1b0fe81f671a26690926bb6e0d75
Aaron Faanes <dafrito@gmail.com>
2013-08-31 03:29:00 -0500

    Removed some old testing code

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
-#include <iterator>
-#include <sstream>
 #include <cassert>
 
 int main(int argc, char* argv[])
 {
-    std::stringstream str;
-    str << "Cat";
-
-    std::istream_iterator<char> eos;
-    std::istream_iterator<char> i(str);
-
-    char a, b;
-    a = *i++;
-    b = *i++;
-    std::cout << a << b << std::endl;
-
     return 0;
 }
 
