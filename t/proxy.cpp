28c04a065c0388f6cb3267ae269f60e947e4f616
Aaron Faanes <dafrito@gmail.com>
2013-09-01 05:38:42 -0500

    Add ProxyRule

---
+++ t/proxy.cpp
@@ -0,0 +1,44 @@
+#include "init.hpp"
+
+#include "DiscardRule"
+#include "TokenRule"
+#include "MultipleRule"
+#include "AlternativeRule"
+#include "SequenceRule"
+#include "ProxyRule"
+
+using namespace sprout;
+
+BOOST_AUTO_TEST_CASE(testTrivialProxy)
+{
+    ProxyRule<char, std::string> rule(
+        OrderedTokenRule<char, std::string>("Cat", "Animal")
+    );
+}
+
+BOOST_AUTO_TEST_CASE(testSplittingByWhitespace)
+{
+    auto rule = makeMultiple(
+        makeSequence<ProxyRule<char, std::string>>(false,
+            makeAlternate(
+                OrderedTokenRule<char, std::string>("Cat", "Heathen"),
+                OrderedTokenRule<char, std::string>("Dog", "Civilized"),
+                OrderedTokenRule<char, std::string>("Calf", "Cow")
+            ),
+            makeDiscard(makeMultiple(
+                AnyTokenRule<char, std::string>("_-")
+            ))
+        )
+    );
+
+    std::stringstream str("Dog___Cat__Calf__Cat");
+    auto cursor = makeCursor<char>(str);
+
+    auto tokens = rule.parse(cursor);
+    BOOST_REQUIRE(tokens);
+    BOOST_CHECK_EQUAL("Civilized", *tokens++);
+    BOOST_CHECK_EQUAL("Heathen", *tokens++);
+    BOOST_CHECK_EQUAL("Cow", *tokens++);
+    BOOST_CHECK_EQUAL("Heathen", *tokens++);
+    BOOST_CHECK(!tokens);
+}
