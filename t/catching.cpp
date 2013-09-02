9d6d73853324af8ecc52510a8cafcfb0f8fcd682
Aaron Faanes <dafrito@gmail.com>
2013-09-02 03:20:26 -0500

    Add CatchingRule, which converts exceptions to failed matches

---
+++ t/catching.cpp
@@ -0,0 +1,38 @@
+#include "init.hpp"
+
+#include "CatchingRule"
+#include "PredicateRule"
+
+using namespace sprout;
+
+struct FakeException {};
+
+BOOST_AUTO_TEST_CASE(testCatching)
+{
+    auto rule = makeCatching<FakeException>(
+        makeAddingPredicate<char, std::string>([](const char& input) {
+            if (input == 'a') {
+                throw FakeException();
+            }
+            return true;
+        })
+    );
+
+    {
+        std::stringstream str("abc");
+        auto cursor = makeCursor<char>(str);
+
+        auto tokens = rule.parse(cursor);
+        BOOST_CHECK(!tokens);
+    }
+
+    {
+        std::stringstream str("bbb");
+        auto cursor = makeCursor<char>(str);
+
+        auto tokens = rule.parse(cursor);
+        BOOST_REQUIRE(tokens);
+        std::cout << *tokens << std::endl;
+        BOOST_CHECK(*tokens == "bbb");
+    }
+}
