9f65fe239de49f22114ea6100dec3dffa510d9d2
Aaron Faanes <dafrito@gmail.com>
2013-09-02 01:18:40 -0500

    PredicateRule, for flexible token generation

---
+++ t/predicate.cpp
@@ -0,0 +1,21 @@
+#include "init.hpp"
+
+#include "PredicateRule"
+
+using namespace sprout;
+
+BOOST_AUTO_TEST_CASE(testPredicate)
+{
+    auto rule = makePredicate<QChar, QString>([](const QChar& c) {
+        return c.isLetter();
+    });
+
+    QString str(QString::fromUtf8("中国"));
+    QTextStream stream(&str);
+    auto cursor = makeCursor<QChar>(stream);
+
+    auto tokens = rule.parse(cursor);
+    BOOST_REQUIRE(tokens);
+    BOOST_CHECK(*tokens == str);
+}
+
