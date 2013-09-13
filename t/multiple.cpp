52beaf361b220e5dfc83470bbdc7eb050971e9a6
Aaron Faanes <dafrito@gmail.com>
2013-09-13 17:01:52 -0500

    MultipleRule no longer depends on RuleTraits

--- t/multiple.cpp
+++ t/multiple.cpp
@@ -49,3 +49,21 @@ BOOST_AUTO_TEST_CASE(matchMultipleWithANonMatch)
 
     BOOST_REQUIRE(!tokens);
 }
+
+BOOST_AUTO_TEST_CASE(matchMultipleWithALambda)
+{
+    auto rule = make::multiple<char, std::string>(
+        [](Cursor<char>& input, Result<std::string>& result) {
+            if (input && *input == 'A') {
+                ++input;
+                return true;
+            }
+            return false;
+        }
+    );
+    Result<std::string> tokens;
+
+    auto cursor = makeCursor<char>("ABC");
+    BOOST_CHECK(rule(cursor, tokens));
+    BOOST_REQUIRE(!tokens);
+}
