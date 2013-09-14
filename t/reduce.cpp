56aad6737436a21adaaa71e9735a843013fc8357
Aaron Faanes <dafrito@gmail.com>
2013-09-14 09:18:30 -0500

    Reducerule no longer depends on RuleTraits being available

---
+++ t/reduce.cpp
@@ -0,0 +1,116 @@
+#include "init.hpp"
+
+#include "ReduceRule"
+#include "MultipleRule"
+#include "AlternativeRule"
+#include "DiscardRule"
+#include "TokenRule"
+
+using namespace sprout;
+
+BOOST_AUTO_TEST_CASE(testReduce)
+{
+    auto rule = make::reduce<std::string>(
+        [](Result<std::string>& result, Result<char>& subResult) {
+            std::string str;
+            for (auto token : subResult) {
+                if (token == '_') {
+                    str += token;
+                }
+            }
+            result << str;
+        },
+        make::multiple(
+            make::alternate(
+                OrderedTokenRule<char, char>("-", '-'),
+                OrderedTokenRule<char, char>("_", '_')
+            )
+        )
+    );
+    Result<std::string> tokens;
+
+    auto cursor = makeCursor<char>("_-_-Dog");
+    BOOST_CHECK(rule(cursor, tokens));
+
+    BOOST_REQUIRE(tokens);
+    BOOST_CHECK_EQUAL("__", *tokens++);
+    BOOST_CHECK(!tokens);
+
+    BOOST_CHECK_EQUAL('D', *cursor);
+}
+
+BOOST_AUTO_TEST_CASE(testReduceWithLambda)
+{
+    auto rule = make::reduce<std::string, char, char>(
+        [](Result<std::string>& result, Result<char>& subResult) {
+            std::string str;
+            for (auto token : subResult) {
+                if (token == '_') {
+                    str += token;
+                }
+            }
+            result << str;
+        },
+        [](Cursor<char>& iter, Result<char>& tokens) {
+            bool found = false;
+            while (iter) {
+                auto c = *iter;
+                if (c != '_' && c != '-') {
+                    break;
+                }
+                found = true;
+                tokens << c;
+                ++iter;
+            }
+            return found;
+        }
+    );
+    Result<std::string> tokens;
+
+    auto cursor = makeCursor<char>("_-_-Dog");
+    BOOST_CHECK(rule(cursor, tokens));
+
+    BOOST_REQUIRE(tokens);
+    BOOST_CHECK_EQUAL("__", *tokens++);
+    BOOST_CHECK(!tokens);
+
+    BOOST_CHECK_EQUAL('D', *cursor);
+}
+
+BOOST_AUTO_TEST_CASE(testReduceWithWrappedRule)
+{
+    auto rule = make::reduce<std::string>(
+        [](Result<std::string>& result, Result<char>& subResult) {
+            std::string str;
+            for (auto token : subResult) {
+                if (token == '_') {
+                    str += token;
+                }
+            }
+            result << str;
+        },
+        make::rule<char, char>([](Cursor<char>& iter, Result<char>& tokens) {
+            bool found = false;
+            while (iter) {
+                auto c = *iter;
+                if (c != '_' && c != '-') {
+                    break;
+                }
+                found = true;
+                tokens << c;
+                ++iter;
+            }
+            return found;
+        })
+    );
+    Result<std::string> tokens;
+
+    auto cursor = makeCursor<char>("_-_-Dog");
+    BOOST_CHECK(rule(cursor, tokens));
+
+    BOOST_REQUIRE(tokens);
+    BOOST_CHECK_EQUAL("__", *tokens++);
+    BOOST_CHECK(!tokens);
+
+    BOOST_CHECK_EQUAL('D', *cursor);
+}
